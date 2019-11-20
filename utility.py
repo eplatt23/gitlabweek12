9e7c443bc57254d8d97f7576f20df46e8d710217
Emmett Platt <eplatt@mymail.mines.edu>
2019-11-20 16:50:13 -0700

    printoutput

---
+++ utility.py
@@ -0,0 +1,9 @@
+#   https://github.com/eplatt23/gitlabweek12.git
+#   Emmett Platt
+#   ​CSCI 102 – Section D
+#   Week 12 - Part B
+    
+
+def PrintOutput(a):
+    a = str(a)
+    print('OUTPUT', a)
